e195d6cf2b91bf2d2eebcebfba932eb3f3e14b23
Dimar <158440383+Dimar1510@users.noreply.github.com>
2024-02-22 14:18:11 +0300

    Minor tweaks to the message system

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ function getPlayerChoice() {
 
 //generate random for computer
 function getComputerChoice() {
-    
     let computerChoice = choices[Math.floor(Math.random()*choices.length)];
     return computerChoice;
 }
@@ -31,38 +30,45 @@ function playRound() {
     let playerSelection = getPlayerChoice();
     let computerSelection = getComputerChoice();
 
+// get messages 
+let tieMessage = `Round ${round}: both picked ${playerSelection}, it's a tie`;
+let winMessage = `Round ${round}: ${playerSelection} beats ${computerSelection}, you win this round`;
+let loseMessage = `Round ${round}: ${computerSelection} beats ${playerSelection}, you lose this round`;
+
 // simple compare
     if (playerSelection === computerSelection) {
-        console.log(`Both picked ${playerSelection}`);
+        console.log(tieMessage);
         return "tie";
     }
     else if (playerSelection === 'rock') {
+
+
         if (computerSelection === 'scissors') {
-            console.log(`Round ${round}: ${playerSelection} beats ${computerSelection}`);
+            console.log(winMessage);
             return "win";
         }
         if (computerSelection === 'paper') {
-            console.log(`Round ${round}: ${computerSelection} beats ${playerSelection}`);
+            console.log(loseMessage);
             return "lose";
         }
     }
     else if (playerSelection === 'paper') {
         if (computerSelection === 'rock') {
-            console.log(`Round ${round}: ${playerSelection} beats ${computerSelection}`);
+            console.log(winMessage);
             return "win";
         }
         if (computerSelection === 'scissors') {
-            console.log(`Round ${round}: ${computerSelection} beats ${playerSelection}`);
+            console.log(loseMessage);
             return "lose";
         }
     }
     else if (playerSelection === 'scissors') {
         if (computerSelection === 'paper') {
-            console.log(`Round ${round}: ${playerSelection} beats ${computerSelection}`);
+            console.log(winMessage);
             return "win";
         }
         if (computerSelection === 'rock') {
-            console.log(`Round ${round}: ${computerSelection} beats ${playerSelection}`);
+            console.log(loseMessage);
             return "lose;"
         }
     }
@@ -78,20 +84,16 @@ function playGame() {
         
         let result = playRound();
         if (result == "win") {
-            console.log("You win this round!");
             playerWin++;
         }
         else if (result == "lose") {
-            console.log("You lose this round!");
             computerWin++;
-        }
-        else {
-            console.log("It's a tie!");
         }
         round++;
            }
+    console.log(`Score: ${playerWin} - ${computerWin}`);
     if (computerWin < playerWin) {
-        console.log("You win the game!");
+        console.log("You won the game!");
     }
     else if (computerWin > playerWin) {
         console.log("You lost the game!");
@@ -99,6 +101,7 @@ function playGame() {
     else {
         console.log("The game is tied!");
     }
+    
 }
 
 console.log(playGame());
